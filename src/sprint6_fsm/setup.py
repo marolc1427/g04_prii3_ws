aa4fb5b243b4f8d4041029fe1532a211aba8ca20
Marcos <marcosoc2005@gmail.com>
2026-03-03 16:09:43 +0100

    Sprint 6

---
+++ src/sprint6_fsm/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'sprint6_fsm'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='marolc',
+    maintainer_email='marcosoc2005@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
